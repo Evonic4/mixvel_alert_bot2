5c6eb8e76a34b4b14c101b02b7d755ada4d0af3c
e.nikitin <e.nikitin@sirena-travel.ru>
2022-05-24 17:45:48 +0300

    mabot2

--- cucu2.sh
+++ cucu2.sh
@@ -8,6 +8,17 @@ Z2="0"
 code1=""
 code2=""
 
+
+function logger()
+{
+local date1=`date '+ %Y-%m-%d %H:%M:%S'`
+
+if [ "$zap" == "1" ]; then
+	echo $date1" cucu2_"$bui": "$1
+fi
+}
+
+
 directly () {
 
 IFS=$'\x10'
@@ -34,9 +45,9 @@ mv $ftb"out0.txt" $ftb"out.txt"
 if ! [ -f $fPID ]; then		#----------------------- старт------------------
 PID=$$
 echo $PID > $fPID
-#echo "start"
+logger "start"
 token=$(sed -n "1p" $ftb"settings.conf" | tr -d '\r')
-
+bui=$(sed -n 11"p" $ftb"settings.conf" | tr -d '\r')
 f_text=$(sed -n "2p" $ftb"send.txt" | tr -d '\r')
 proxy=$(sed -n 5"p" $ftb"settings.conf" | tr -d '\r')
 bicons=$(sed -n 19"p" $ftb"settings.conf" | tr -d '\r')
@@ -45,15 +56,18 @@ sty=$(sed -n 20"p" $ftb"settings.conf" | tr -d '\r')
 [ "$bicons" == "1" ] && Z1=$1
 [ "$sty" == "1" ] && Z2=$2
 
+logger "Z1="$Z1
+logger "Z2="$Z2
+
 [ "$Z1" == "1" ] && code1="&#10060;"
 [ "$Z1" == "2" ] && code1="&#9989"
 
 str_col=$(grep -cv "^#" $ftb"chats.txt")
-#echo "str_col="$str_col
+logger "str_col="$str_col
 if [ "$str_col" -gt "0" ]; then
 for (( i=1;i<=$str_col;i++)); do
 	chat_id=$(sed -n $i"p" $ftb"chats.txt" | sed 's/z/-/g' | tr -d '\r')
-	#echo "chat_id="$chat_id
+	logger "chat_id="$chat_id
 	directly;
 done
 fi
